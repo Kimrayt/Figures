e7fc824afeaa90ff5a7ef9c1c6b5f852efb788fc
Kimrayt <kimrayt61@gmail.com>
2022-04-27 17:12:23 +0300

    Completed serialization suing objectStream. But using JSON is yet to be done

---
+++ src/com/company/Point.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class Point implements Serializable {
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public String toString (){
+        return "Point: "+this.getX() + "; "+ this.getY();
+    }
+}
